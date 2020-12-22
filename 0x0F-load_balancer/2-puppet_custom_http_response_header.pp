8dd1fa5db00af5182109cd7236c688875c1470f9
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-12-22 07:48:07 +0000

    Advanced Task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# creating a custom HTTP header response, but with Puppet.
+
+exec { 'Update':
+  command => 'sudo apt-get update',
+  path    => ['/usr/bin'],
+}
+
+package { 'Install nginx':
+  ensure  => installed,
+  require => Exec['update'],
+  name    => 'nginx'
+}
+
+file_line { 'append line in nginx config file':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server',
+  line   => "\tadd_header X-Served-By ${hostname};",
+}
+
+exec { 'restart nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin'],
+}
