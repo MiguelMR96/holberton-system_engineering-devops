98201de0eaeb09f76ff34e43d7194ec5ba5f789e
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-12-22 08:25:14 +0000

    Advanced Task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,23 @@
 # creating a custom HTTP header response, but with Puppet.
 
 exec { 'apt-get update':
-  command => 'sudo apt-get update',
-  path    => ['/usr/bin', '/bin'],
+command => 'sudo apt-get update',
+path    => ['/usr/bin', '/bin'],
 }
 
 package { 'install nginx':
-  ensure  => installed,
-  require => Exec['update'],
+ensure => installed,
+name   => 'nginx',
 }
 
 file_line { 'append line in nginx config file':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => "add_header X-Served-By ${hostname};",
+ensure => present,
+path   => '/etc/nginx/sites-available/default',
+after  => 'server_name _;',
+line   => "add_header X-Served-By ${hostname};",
 }
 
 exec { 'restart nginx':
-  command => 'sudo service nginx restart',
-  path    => ['/usr/bin', '/bin'],
+command => 'sudo service nginx restart',
+path    => ['/usr/bin', '/bin'],
 }
