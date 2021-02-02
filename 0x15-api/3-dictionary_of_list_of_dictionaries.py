78e4274c3d5cb63e4055ba264b103d3bb3b5eb70
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-02 00:09:01 -0500

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -23,7 +23,6 @@ if __name__ == "__main__":
         'https://jsonplaceholder.typicode.com/todos').json()
 
     # Extracting data from json
-    name = users[0].get('name')
     with open('todo_all_employees.json', 'w') as file_json:
         # Go through all users extracting data
         for iterator in users:
@@ -33,11 +32,12 @@ if __name__ == "__main__":
             # Go to every employee tasks, asign the values and append it
             # in the list of the employee's tasks (employee_list)
             for inner_iter in todos:
-                tasks_dic = {}
-                tasks_dic['username'] = username
-                tasks_dic['task'] = inner_iter.get('title')
-                tasks_dic['completed'] = inner_iter.get('completed')
-                employee_list.append(tasks_dic)
+                if employee_id == inner_iter.get('userId'):
+                    tasks_dic = {}
+                    tasks_dic['username'] = username
+                    tasks_dic['task'] = inner_iter.get('title')
+                    tasks_dic['completed'] = inner_iter.get('completed')
+                    employee_list.append(tasks_dic)
             # Finally asign the employee id in the dictionary where
             # all employees are stored (dic_employees) and then
             # the dictionary is stored in JSON format-file.
