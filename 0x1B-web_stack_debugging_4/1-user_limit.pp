44daeca9926c658b50a90f7cb943d4dd55711ec8
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-25 14:19:17 -0500

    Task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,4 @@
-# Puppet that change the limit of open files in an Nginx server.
+#Configure limits
+exec { 'change-os-configuration':
+  command => "/bin/sed -i -e 's/nofile 5/nofile 1000/g;s/nofile 4/nofile 1000/g' /etc/security/limits.conf"
+}
